3491e5927f49c9ff274b1b850381d3463c86e6d7
Kajal Chingsubam <kchings1@binghamton.edu>
2021-03-03 04:41:51 -0500

    data entry and fetch works

--- src/Form.js
+++ src/Form.js
@@ -13,31 +13,45 @@ const Form =()=>{
     const [notes, setNotes]=useState([])
 
 
+   const SubmitHandler = ()=>{
+
+        firebase.db.collection("todos").add({
+            todo: note
+        })
+       setNotes([...notes, note])
+       setNote("")
+   }
+
+
+ 
     
 
-    const ClickHandler=()=>{
-            
-            setNotes([...notes, note])
-            
-            setNote("")
-            
-    }
-
-    useEffect(()=>{
-        firebase.db.collection("Ching Notes: ").add({
-            list: notes
-        }
-            )
-    }, [notes])
+  
+    useEffect( ()=>
+      { firebase.db.collection('todos').onSnapshot(snap=>{
+        const arr=snap.docs.map(todo=>{
+            const data=todo.data()
+         //    console.log("data: ",data.todo)
+         return data.todo
+        })
+   
+        setNotes(arr)
+    })
 
 
+      },[]  
+    )
 
+  
+    
     return (
 
         <div>
             <input type = "text" value={note} onChange={(e)=>setNote(e.target.value)} />
-            <button onClick={ClickHandler}>Submit</button>
+            <button onClick={SubmitHandler} >Submit</button>
+           
             <ViewSpace notes={notes}/>
+            {/* <button onClick={FetchHandler}>Fetch</button> */}
             
         </div>
     )
