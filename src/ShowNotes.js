d09eb6cf3144239f506ac1c7651f7c10823f1a63
Kajal Chingsubam <kchings1@binghamton.edu>
2021-03-05 19:03:34 -0500

    added del btn

--- src/ShowNotes.js
+++ src/ShowNotes.js
@@ -1,9 +1,30 @@
 import React from 'react';
-
+import firebase from './firebase'
 const ShowNotes = ({item})=>{
     
+    // const DeleteHandler = (item)=>{
+    //     firebase.db.collection('todos').onSnapshot(snap=>{
+    //         const arr=snap.docs.map(todo=>{
+    //             const data=todo.data()
+    //             // console.log("id: ",todo.id)
+    //             if (data.todo == item){
+    //                 return todo.id
+    //             }
+             
+    //         })
+    //         console.log(`item id:${arr}` )
+               
+    //     }
+       
+    //     )
+    
+    // }  
+
     return (
-       <li>{item}</li>
+        <div>
+        <li>{item} <button onClick={()=>alert(`delete item ${item} `)}>del</button>
+        </li>
+       </div>
     )
 
 }
