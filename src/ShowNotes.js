7bd6c4bdff48d5d478ca2b8adea29b07ba351eb8
Kajal Chingsubam <kchings1@binghamton.edu>
2021-03-09 03:21:02 -0500

    added edit prototype with an icon

--- src/ShowNotes.js
+++ src/ShowNotes.js
@@ -1,10 +1,16 @@
 import { Button, ListItem, ListItemText } from '@material-ui/core';
 import React from 'react';
-import firebase from './firebase'
+import firebase from './firebase';
+import EditIcon from '@material-ui/icons/Edit';
 const ShowNotes = ({todo,id})=>{
     
    const DelHandler = ()=>{
        firebase.db.collection("todos").doc(id).delete()
+   }
+   const UpdateHandler=()=>{
+    firebase.db.collection("todos").doc(id).update({
+        todo:"new edited"
+    })
    }
 
     return (
@@ -15,6 +21,8 @@ const ShowNotes = ({todo,id})=>{
         </ListItem>
         <div style={{maxWidth:"15px", marginTop:"5px"}}>
         <Button variant="contained" size="small" color="secondary"  style={{color:"white"}} onClick={DelHandler}>del</Button>
+        <EditIcon onClick={UpdateHandler}  ></EditIcon>
+
         </div>
         {/* <ListItem>
         <ListItemText i primary={` ${todo}`} />
